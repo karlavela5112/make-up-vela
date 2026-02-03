ab4a28d5e8616e8b46e6a6cc67332c9ba326d67c
aylinperez777 <isis.perez5178@alumnos.udg.mx>
2026-02-03 11:03:49 -0600

    block_p5

--- makeupvela.py
+++ makeupvela.py
@@ -1,3 +1,11 @@
-from flask import Flask
+from tempfile import template
+from Flask import Flask, rendertemplate,url_for
 
-makeupvelaApp = Flask (__name__)
+makeupvelaApp=Flask (__name__)
+
+@makeupvelaApp.route('/')
+def home():
+return '<h1> Consigue tu maquillaje con precio accesible <h1>'
+
+if __name__ == '_main_':
+    makeupvelaApp.run(port=2200,debug=True)

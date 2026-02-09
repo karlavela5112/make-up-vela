c42036d2c82fce3c277e7016a7f86da48353bbf8
karlavela5112 <karla.vela5112@alumnos.udg.mx>
2026-02-09 10:48:41 -0600

    signup_p7

--- makeupvela.py
+++ makeupvela.py
@@ -1,11 +1,28 @@
 from tempfile import template
-from Flask import Flask, rendertemplate,url_for
+from flask import Flask, render_template, url_for requst
+from werkzeug.security import generate_password_hash
 
 makeupvelaApp=Flask (__name__)
 
 @makeupvelaApp.route('/')
 def home():
-return '<h1> Consigue tu maquillaje con precio accesible <h1>'
-
-if __name__ == '_main_':
-    makeupvelaApp.run(port=2200,debug=True)
+    ""
+    return render_template('home.html')
+@oldwearApp.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.form == 'POST':
+        nombre = request.form['nombre']
+        correo = request.form['correo'] \
+        clave = request.form['clave']
+        claveCifrada = generate_password_hash(clave)
+        regUsuario = db.conection.cursor()
+        regUsuario = db.execute("INSERT INTO usuarios (nombre, correo, clave) VALUES (%s, %s, %s)", (nombre.upper(), correo, claveCifrada))
+        db.conection.commit()
+        regUsuario.close()
+        return redirect(url_for'home.html')
+    else:
+        """Renderiza el formulario de registrO"""
+        return render_template('registro.html')
+    
+if __name__ == '__main__':
+    makeupvelaApp.run(debug=True)

7db02ae3d16eace23877420c2523af2cb80d7d78
karlavela5112 <karla.vela5112@alumnos.udg.mx>
2026-02-18 12:26:10 -0600

    signin

--- makeupvelaApp.py
+++ makeupvelaApp.py
@@ -3,11 +3,18 @@ from flask import Flask, render_template, url_for, request
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash
 from config import config
+from models.entities.User import User
+from models.ModelUser import ModelUser
+from flask_login import LoginManager, login_user, logout_user
 
 makeupvelaApp=Flask (__name__)
 
 db              =MySQL(makeupvelaApp)
+adminUsuario = LoginManager(makeupvelaApp)
 
+@adminUsuario.user_loader
+def cargarUsuario(id):
+    return ModelUser.get_by_id(db, id)
 @makeupvelaApp.route('/')
 def home():
     ""
@@ -27,7 +34,27 @@ def signup():
     else:
         
         return render_template('registro.html')
-    
-if __name__ == '__main__':
+
+@makeupvelaApp.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        usuario = User(0,None,request.form['correo'],request.form['clave'],None)
+        usuarioAutenticado = ModelUser.signin(db, usuario)
+        if usuarioAutenticado is not None:
+            if usuarioAutenticado.clave:
+                login_user(usuarioAutenticado)
+                if usuarioAutenticado.perfil == 'A':
+                    return render_template('admin.html')
+                else:
+                    return render_template('usuario.html')
+            else:
+                return 'Contraseña incorrecta'
+        else:
+            return 'Usuario inexistente'
+    else:
+        return render_template('signin.html')            
+
+
+ if __name__ == '__main__':
     makeupvelaApp.config.from_object(config['development'])
     makeupvelaApp.run(port=5025,debug=True)
